0a0e69cf9a6b4f45ac2ec0787a6af2531ff6a2a9
Otim <53155760+zztim888@users.noreply.github.com>
2022-11-02 22:34:47 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Nested from "./Nested.svelte";
+
+  const bio = {
+    name: 'Kevin',
+    age: '33'
+  }
+</script>
+
+<Nested name={bio.name} age={bio.age} />
---
+++ src/Nested.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let name;
+    export let age;
+</script>
+
+<p>he's name is {name} and he is {age} years old.</p>
